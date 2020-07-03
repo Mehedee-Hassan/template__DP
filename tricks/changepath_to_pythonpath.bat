8da788423a7d45ebb68b1074ab68c49a74e7a532
Mehedee <razin.meha@gamil.com>
2020-07-03 13:58:16 +0900

    change windows path to python path

--- tricks/changepath_to_pythonpath.bat
+++ tricks/changepath_to_pythonpath.bat
@@ -6,3 +6,18 @@ call set path=%%path:\=%word%%%
 echo %path%
 
 pause
+
+
+:: if you want to run from python with console
+::# convert file 
+::# """
+::# if you need to change path format 
+
+
+::from subprocess import Popen
+::import subprocess
+::def run_batch_file(file_path):
+::    Popen(file_path,creationflags=subprocess.CREATE_NEW_CONSOLE)
+::run_batch_file([r'C:\Users\mehedee\Documents\Python Scripts\mycode_tepmlate\tricks\changepath_to_pythonpath.bat'])
+
+::"""
